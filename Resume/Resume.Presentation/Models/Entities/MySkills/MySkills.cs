48402b5f54e52e3b26bdaed3edfa966a14cf0c03
Mahnoosh <mahnooshn0@gmail.com>
2023-10-20 23:34:01 +0330

    MySkill

---
+++ Resume/Resume.Presentation/Models/Entities/MySkills/MySkills.cs
@@ -0,0 +1,8 @@
+﻿namespace Resume.Presentation.Models.Entities.MySkills;
+
+public class MySkills
+{
+    public int Id { get; set; }
+    public string SkillTitle { get; set; }
+    public int Persentage { get; set; }
+}
