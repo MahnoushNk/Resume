646843866f3314e0d39ff445ce217a9f7c43bf48
Mahnoosh <mahnooshn0@gmail.com>
2023-10-20 16:52:21 +0330

    start project

---
+++ Resume/Resume.Presentation/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Presentation.Controllers
+{
+    public class AdminController : Controller
+    {
+        #region Admin Dashboard
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string Email, string UserName)
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region List Of Usesr
+
+        [HttpPost]
+        public IActionResult ListOfUsers(string Email, string UserName)
+        {
+            return View();
+        }
+
+        #endregion
+    }
+
+}
+
