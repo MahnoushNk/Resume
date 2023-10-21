80b951f4c7294825126ab413ea0500cd7439e13b
Mahnoosh <mahnooshn0@gmail.com>
2023-10-21 10:04:10 +0330

    Add Education Controller

---
+++ Resume/Resume.Presentation/Controllers/EducationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Presentation.Controllers
+{
+    public class EducationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
